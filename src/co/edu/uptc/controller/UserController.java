27a69fe44f63eb27a0313c7c23582d9f164b22e5
ROBINSON MOLINA <robinson.molina01@uptc.edu.co>
2023-11-11 02:33:42 -0500

    Second change

---
+++ src/co/edu/uptc/controller/UserController.java
@@ -0,0 +1,5 @@
+package co.edu.uptc.controller;
+
+public class UserController {
+    
+}
---
+++ src/co/edu/uptc/model/Admin.java
@@ -0,0 +1,6 @@
+package co.edu.uptc.model;
+
+public class Admin {
+    private int user;
+    private int password;
+}
