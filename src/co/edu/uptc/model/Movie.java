b18456898b5e372fe2dff60e4fd2247f43c534fe
ROBINSON MOLINA <robinson.molina01@uptc.edu.co>
2023-11-24 14:26:20 -0500

    Add utilitaries

--- src/co/edu/uptc/model/Movie.java
+++ src/co/edu/uptc/model/Movie.java
@@ -2,22 +2,22 @@ package co.edu.uptc.model;
 
 import java.util.ArrayList;
 
-public class Movie extends Film{
+public class Movie {
     private String name;
     private String description;
-    private String duration;
+    private int duration;
     private ArrayList<Movie> listMovies;
-    private String listActors;
+    private ArrayList<String> listActors;
     private String gender;
 
     public Movie() {
     }
 
-    public Movie(String name, String description, String duration, String string, String gender) {
+    public Movie(String name, String description, int duration, ArrayList<String> listActors, String gender) {
         this.name = name;
         this.description = description;
         this.duration = duration;
-        this.listActors = string;
+        this.listActors = listActors;
         this.gender = gender;
     }
     public String getName() {
@@ -32,16 +32,16 @@ public class Movie extends Film{
     public void setDescription(String description) {
         this.description = description;
     }
-    public String getDuration() {
+    public int getDuration() {
         return duration;
     }
-    public void setDuration(String duration) {
+    public void setDuration(int duration) {
         this.duration = duration;
     }
-    public String getListActors() {
+    public ArrayList<String> getListActors() {
         return listActors;
     }
-    public void setListActors(String listActors) {
+    public void setListActors(ArrayList<String> listActors) {
         this.listActors = listActors;
     }
     
@@ -59,11 +59,6 @@ public class Movie extends Film{
 
     public void setGender(String gender) {
         this.gender = gender;
-    }
-    
-    @Override
-    public void AddDates() {
-        listMovies.add(new Movie(name, description, duration, listActors, gender));
     }
 
     @Override
--- src/co/edu/uptc/model/Serie.java
+++ src/co/edu/uptc/model/Serie.java
@@ -2,11 +2,11 @@ package co.edu.uptc.model;
 
 import java.util.ArrayList;
 
-public class Serie extends Film{
+public class Serie {
     private String name;
     private String description;
-    private String duration;
-    private String listActors;
+    private int duration;
+    private ArrayList<String> listActors;
     private ArrayList<Serie> listSeries;
     private String listChapters;
     private String gender;
@@ -14,7 +14,7 @@ public class Serie extends Film{
     public Serie() {
     }
     
-    public Serie(String name, String description, String duration, String listActors,
+    public Serie(String name, String description, int duration, ArrayList<String> listActors,
             String listChapters, String gender) {
         this.name = name;
         this.description = description;
@@ -35,16 +35,16 @@ public class Serie extends Film{
     public void setDescription(String description) {
         this.description = description;
     }
-    public String getDuration() {
+    public int getDuration() {
         return duration;
     }
-    public void setDuration(String duration) {
+    public void setDuration(int duration) {
         this.duration = duration;
     }
-    public String getListActors() {
+    public ArrayList<String> getListActors() {
         return listActors;
     }
-    public void setListActors(String listActors) {
+    public void setListActors(ArrayList<String> listActors) {
         this.listActors = listActors;
     }
     public String getListChapters() {
@@ -67,10 +67,6 @@ public class Serie extends Film{
 
     public void setGender(String gender) {
         this.gender = gender;
-    }
-    @Override
-    public void AddDates() {
-        listSeries.add(new Serie(name, description, duration, listActors, listChapters, gender));
     }
 
     @Override
