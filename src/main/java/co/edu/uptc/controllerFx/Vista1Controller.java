d043eedfc2fd69190eec667b02a4a4fd71e1dbb9
JuanBarrer4 <juan.barrera16@uptc.edu.co>
2024-03-12 04:57:56 -0500

    se termina suscription y validaciones

--- src/main/java/co/edu/uptc/controllerFx/Vista1Controller.java
+++ src/main/java/co/edu/uptc/controllerFx/Vista1Controller.java
@@ -11,6 +11,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+
+import co.edu.uptc.controller.ControlerInitialMenuView;
 import co.edu.uptc.model.User;
 
 public class Vista1Controller {
@@ -38,12 +42,17 @@ public class Vista1Controller {
 
     @FXML
     private void initialize() {
-        option1.setOnAction(event -> abrirVista("/co/edu/uptc/Fxml/MovieCatalogView.fxml", new MovieCatalogController(user)));
-        option2.setOnAction(event -> abrirVista("/co/edu/uptc/Fxml/SerieCatalogView.fxml", new SerieCatalogController(user)));
+
+        option1.setOnAction(
+                event -> abrirVista("/co/edu/uptc/Fxml/MovieCatalogView.fxml", new MovieCatalogController(user)));
+        option2.setOnAction(
+                event -> abrirVista("/co/edu/uptc/Fxml/SerieCatalogView.fxml", new SerieCatalogController(user)));
         option3.setOnAction(event -> abrirVista("/co/edu/uptc/Fxml/Search.fxml", new SearchController(user)));
-        // option4.setOnAction(event -> abrirVista("/co/edu/uptc/Fxml/Favorites.fxml", new FavoritesController(user)));
+        // option4.setOnAction(event -> abrirVista("/co/edu/uptc/Fxml/Favorites.fxml",
+        // new FavoritesController(user)));
         option5.setOnAction(event -> abrirVista("/co/edu/uptc/Fxml/Settings.fxml", new SettingsController(user)));
-        option6.setOnAction(event -> abrirVista("/co/edu/uptc/Fxml/Suscription.fxml", new SuscriptionController(user)));
+        option6.setOnAction(
+                event -> abrirVistaJuan("/co/edu/uptc/Fxml/Suscription.fxml", new SuscriptionController(user)));
         welcomeLabel.setText(user.getFirstName());
 
     }
@@ -66,7 +75,7 @@ public class Vista1Controller {
     }
 
     @FXML
-    public void handleFavoriteView(){
+    public void handleFavoriteView() {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/co/edu/uptc/Fxml/Favorites.fxml"));
             Parent root = loader.load();
@@ -90,23 +99,80 @@ public class Vista1Controller {
     }
 
     private void abrirVista(String fxmlPath, Object controller) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlPath));
-            fxmlLoader.setController(controller);
-            if (controller instanceof FavoritesController) {
-                ((FavoritesController) controller).setUser(user);
+           System.out.println(user.getActieSusciption());
+        if (!user.getActieSusciption().equals("active")) {
+            JOptionPane.showMessageDialog(null, " no tiene suscripción  Activa");
+
+        }else{
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlPath));
+                fxmlLoader.setController(controller);
+                if (controller instanceof FavoritesController) {
+                    ((FavoritesController) controller).setUser(user);
+                }
+                Parent root = fxmlLoader.load();
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.setTitle("User Register");
+                stage.setScene(scene);
+                stage.show();
+                // Cerrar la ventana actual
+                Stage myStage = (Stage) this.option1.getScene().getWindow();
+                myStage.close();
+            } catch (IOException e) {
+                Logger.getLogger(Vista1Controller.class.getName()).severe(e.getMessage());
             }
-            Parent root = fxmlLoader.load();
-            Scene scene = new Scene(root);
-            Stage stage = new Stage();
-            stage.setTitle("User Register");
-            stage.setScene(scene);
-            stage.show();
-            // Cerrar la ventana actual
-            Stage myStage = (Stage) this.option1.getScene().getWindow();
-            myStage.close();
-        } catch (IOException e) {
-            Logger.getLogger(Vista1Controller.class.getName()).severe(e.getMessage());
         }
+       
+    }
+
+    private void abrirVistaJuan(String fxmlPath, Object controller) {
+       
+        if (user.getActieSusciption().equals("active")) {
+            JOptionPane.showMessageDialog(null, " tiene suscripcon activa ");
+        }else{
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlPath));
+                fxmlLoader.setController(controller);
+                if (controller instanceof FavoritesController) {
+                    ((FavoritesController) controller).setUser(user);
+                }
+                // el objeto user que necesitamos
+                Parent root = fxmlLoader.load();
+    
+                TextField email = (TextField) fxmlLoader.getNamespace().get("email");
+                TextField DateFin = (TextField) fxmlLoader.getNamespace().get("DateFin");
+                
+    
+                email.setText(user.getEmail());
+                email.setEditable(false);
+                // creo la clase de controlador de la logica
+                   ControlerInitialMenuView controlerInitialMenuView= new ControlerInitialMenuView(); 
+            try {
+                // leo los archivos user 
+                controlerInitialMenuView.getUsers().addAll(controlerInitialMenuView.loadUsers("Users"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            // asigno la fecha la feccha 
+                user.setFinDateDeSubscription(controlerInitialMenuView.dateFinSuscripcion(user));
+                // darle formato 
+                String fechaFinSus= controlerInitialMenuView.formatearFecha(user.getFinDateDeSubscription());
+                DateFin.setText(fechaFinSus);
+                DateFin.setEditable(false);
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.setTitle("User Register");
+                stage.setScene(scene);
+                stage.show();
+                // Cerrar la ventana actual
+                Stage myStage = (Stage) this.option1.getScene().getWindow();
+                myStage.close();
+            } catch (IOException e) {
+                Logger.getLogger(Vista1Controller.class.getName()).severe(e.getMessage());
+            }
+        }
+        
+       
     }
 }
